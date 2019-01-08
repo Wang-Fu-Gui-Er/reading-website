2253d760436acc78f57f31aa75a5e1c30ee83b27
yx8102 <yx1912246570@qq.com>
2019-01-08 16:01:46 +0800

    project start

---
+++ reading-website-biz/src/main/java/com/reading/website/biz/ReadingWebsiteBizApplication.java
@@ -0,0 +1,14 @@
+package com.reading.website.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingWebsiteBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadingWebsiteBizApplication.class, args);
+    }
+
+}
+
