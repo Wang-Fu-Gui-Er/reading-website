8968441b32eb03048b8d8a0bb12194d53f2e9eb5
yx8102 <1912246570@qq.com>
2019-01-21 13:38:42 +0800

    add @Data

--- reading-website-api/src/main/java/com/reading/website/api/domain/UserBaseInfoDO.java
+++ reading-website-api/src/main/java/com/reading/website/api/domain/UserBaseInfoDO.java
@@ -1,13 +1,9 @@
 package com.reading.website.api.domain;
 
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import org.springframework.beans.BeanUtils;
-import org.springframework.util.CollectionUtils;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -17,12 +13,12 @@ import java.util.List;
  * @yx8102 2019/1/7
  */
 @ApiModel(value="用户对象")
+@Data
 public class UserBaseInfoDO implements Serializable {
 
     /**
      * 主键userId
      */
-    @ApiModelProperty(value="用户id")
     private Long id;
 
     /**
@@ -85,107 +81,4 @@ public class UserBaseInfoDO implements Serializable {
      */
     private Boolean isAdmin;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public List<Long> getIds() {
-        return ids;
-    }
-
-    public void setIds(List<Long> ids) {
-        this.ids = ids;
-    }
-
-    public Date getCreated() {
-        return created;
-    }
-
-    public void setCreated(Date created) {
-        this.created = created;
-    }
-
-    public Date getUpdated() {
-        return updated;
-    }
-
-    public void setUpdated(Date updated) {
-        this.updated = updated;
-    }
-
-    public Boolean getDeleted() {
-        return isDeleted;
-    }
-
-    public void setDeleted(Boolean deleted) {
-        isDeleted = deleted;
-    }
-
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getMobileNum() {
-        return mobileNum;
-    }
-
-    public void setMobileNum(String mobileNum) {
-        this.mobileNum = mobileNum;
-    }
-
-    public String getWeChatId() {
-        return weChatId;
-    }
-
-    public void setWeChatId(String weChatId) {
-        this.weChatId = weChatId;
-    }
-
-    public String getWeiboName() {
-        return weiboName;
-    }
-
-    public void setWeiboName(String weiboName) {
-        this.weiboName = weiboName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getHeadPicPath() {
-        return headPicPath;
-    }
-
-    public void setHeadPicPath(String headPicPath) {
-        this.headPicPath = headPicPath;
-    }
-
-    public Boolean getIsAdmin() {
-        return isAdmin;
-    }
-
-    public void setIsAdmin(Boolean admin) {
-        isAdmin = admin;
-    }
 }
