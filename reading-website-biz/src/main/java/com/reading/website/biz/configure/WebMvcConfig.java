6a41996f8af985f296520ac9d8c19d288973af1a
yx8102 <1912246570@qq.com>
2019-03-28 12:27:46 +0800

    增加跨域配置

---
+++ reading-website-biz/src/main/java/com/reading/website/biz/configure/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.reading.website.biz.configure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 跨域配置
+ *
+ * @xyang010 2019/3/28
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
