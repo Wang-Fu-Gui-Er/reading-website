77c7848c933389d170cad9c442a7a715de66166f
xyang010 <xyang010@mail.nfsq.com.cn>
2019-05-21 21:28:58 +0800

    revert 跨域

--- reading-website-biz/src/main/java/com/reading/website/biz/configure/WebMvcConfig.java
+++ reading-website-biz/src/main/java/com/reading/website/biz/configure/WebMvcConfig.java
@@ -18,18 +18,18 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginInterceptorHandler loginInterceptorHandler;
 
-//    /**
-//     * 跨域配置
-//     * @param registry
-//     */
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
-//                .maxAge(3600)
-//                .allowCredentials(true);
-//    }
+    /**
+     * 跨域配置
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
 
     /**
      * 注册拦截器,自己写好的拦截器需要通过这里添加注册才能生效
